46565ab665f60a5cbd1232f92158f96b0bbf4da2
Arno Goebel <goebel.arno@gmail.com>
2021-06-11 14:28:08 +0200

    config changed

--- config/config.js
+++ config/config.js
@@ -13,18 +13,26 @@ const config = {
     },
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "username": process.env.PGUSER,
+    "password": process.env.PGPW,
+    "database": process.env.PGDB,
+    "host": process.env.PGHOST,
+    "port": process.env.PGPORT,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "useUTC": true, // -->Add this line. for reading from database
+    },
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "username": process.env.PGUSER,
+    "password": process.env.PGPW,
+    "database": process.env.PGDB,
+    "host": process.env.PGHOST,
+    "port": process.env.PGPORT,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "useUTC": true, // -->Add this line. for reading from database
+    },
   }
 }
 
