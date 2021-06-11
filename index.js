4710021883460ed460854ca5836e774308b9f5d3
Arno Goebel <goebel.arno@gmail.com>
2021-06-11 14:43:04 +0200

    changed output of main route

--- index.js
+++ index.js
@@ -14,7 +14,21 @@ app.use(userRouter)
 app.use(messageRouter)
 
 app.get('/', (req, res, next) => {
-    res.send('BAckend-team, wohooooo')
+    res.send(
+        <div>
+            <h1>Routes:</h1>
+            <p>GET https://protected-wave-89943.herokuapp.com/users</p>
+            <p>GET https://protected-wave-89943.herokuapp.com/users/:id</p>
+            <p>GET https://protected-wave-89943.herokuapp.com/users/:id/messages</p>
+            <br />
+            <p>POST https://protected-wave-89943.herokuapp.com/users</p>
+            <br />
+            <p>GET https://protected-wave-89943.herokuapp.com/messages</p>
+            <p>GET https://protected-wave-89943.herokuapp.com/messages/:id</p>
+            <br />
+            <p>POST https://protected-wave-89943.herokuapp.com/messages</p>
+        </div>
+    )
 })
 
 models
